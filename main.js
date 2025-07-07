01a9434c1bf0b176ce70e519e25676f89b55dfe7
bailsone <pmoede@gmail.com>
2025-07-08 00:04:18 +0200

    INIT

---
+++ main.js
@@ -0,0 +1,143 @@
+/*
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
+  */
+
+//Import the THREE.js library
+import * as THREE from 'three';
+// To allow for the camera to move around the scene
+import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "/node_modules/three/examples/jsm/loaders/GLTFLoader.js";
+
+
+//Create a Three.JS Scene
+const scene = new THREE.Scene();
+//create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+THREE.PerspectiveCam
+//Keep track of the mouse position, so we can make the eye move
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+//Keep the 3D object on a global variable so we can access it later
+let object;
+
+//OrbitControls allow the camera to move around the scene
+let controls;
+
+//Set which object to render
+let objToRender = 'kroete';
+
+//Instantiate a loader for the .gltf file
+const loader = new GLTFLoader();
+
+//Load the file
+loader.load(
+  `./assets/kroete/scene.gltf`,
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    object = gltf.scene;
+    scene.add(object);
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
+  }
+);
+
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//Add the renderer to the DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+//Set how far the camera will be from the 3D model
+camera.position.z = objToRender === "kroete" ? 25 : 1500;
+
+//Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, -500) //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+THREE.Scene
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "kroete" ? 5 : 1);
+scene.add(ambientLight);
+//This adds controls to the camera, so we can rotate / zoom it with the mouse
+
+if (objToRender === "kroete") {
+  //controls = new OrbitControls(camera, renderer.domElement)
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true;   //damping 
+        controls.dampingFactor = 0.25;   //damping inertia
+        controls.enableZoom = true;      //Zooming
+        controls.autoRotate = false;       // enable rotation
+        controls.maxPolarAngle = Math.PI / 1.5; // Limit angle of visibility
+        controls.maxDistance = 100.0;
+        controls.minDistance = 50.0;
+        controls.keys = {
+          LEFT: 37, //left arrow
+          UP: 38, // up arrow
+          RIGHT: 39, // right arrow
+          BOTTOM: 40 // down arrow
+        };
+}
+
+//Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  //Here we could add some code to update the scene, adding some automatic movement
+  object.rotation.y += 0.001;
+  //Make the eye move
+  if (object && objToRender === "eye") {
+    //I've played with the constants here until it looked good
+    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
+    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
+  }
+  renderer.render(scene, camera);
+}
+
+//Add a listener to the window, so we can resize the window and the camera
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+//add mouse position listener, so we can make the eye move
+document.onmousemove = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+}
+
+//Start the 3D rendering
+animate();
